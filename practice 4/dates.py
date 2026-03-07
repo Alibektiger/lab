264c89952137b78e8430358719457e8a4053d6e9
Alibektiger <al_aibekuly@kbtu.kz>
2026-03-07 10:41:31 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ practice 4/dates.py
@@ -0,0 +1,21 @@
+#1 
+import datetime
+x=datetime.datetime.now()
+print(x)
+#2
+import datetime
+x=datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))
+#3
+import datetime
+x=datetime.datetime(2020,5,17)
+print(x)
+#4
+x=datetime.datetime(2018,6,1)
+print(x.strftime("%B"))
+#5
+from datetime import datetime
+date_string="16-02-2026"
+dt=datetime.strptime(date_string,"%d-%m-%Y")
+print(dt)
